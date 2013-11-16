e5361b5ca1376122d0280d18b00f04638142ccdd
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2013-11-16 15:06:24 +0700

    First Page and Effect

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,17 @@
-
+$( document ).ready(function() {
+    $.stellar({
+  		horizontalScrolling: false,
+  		positionProperty: 'transform'
+    });
+    $(window).scroll(function(e){ 
+		var topbar = $('.top-bar');
+		if ($(this).height()-$(this).scrollTop() <= 53){ 
+			topbar.css({'position': 'fixed', 'top': '0px'}); 
+		} else {
+			topbar.css({
+				'position': 'absolute',
+				'top': 'auto'
+			});
+		}
+	});
+});
