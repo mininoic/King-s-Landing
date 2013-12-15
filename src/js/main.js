44c5a7dff37597021cb6c978b8c1fc019a58248f
Nguyen Tuan Anh <tebaomang@Nguyens-MacBook-Pro.local>
2013-12-16 00:41:45 +0700

    super fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,60 +1,43 @@
 $( document ).ready(function() {
-    $.stellar({
-  		horizontalScrolling: false,
-  		positionProperty: 'transform'
-    });
+    // $.stellar({
+  		// horizontalScrolling: false,
+  		// positionProperty: 'transform'
+    // });
     var preventDefault = function(selector){
     	$(selector).on("click", function (e) {
 	        e.preventDefault();
 	    });
     }
     preventDefault('.top-bar a');
+    
+    var topbar = $('.top-bar');
+    
     $(window).scroll(function(e){ 
-		var topbar = $('.top-bar');
-		if ($(this).height()-$(this).scrollTop() <= 53){ 
-			topbar.css({'position': 'fixed', 'top': '0px'}); 
-            topbar.removeClass('on-post0');
+		
+		if ($(this).height()-$(this).scrollTop() <= topbar.height()){
+            topbar.addClass('top-bar-top');
 		} else {
-			topbar.css({
-				'position': 'absolute',
-				'top': 'auto'
-			});
-            topbar.addClass('on-post0');
+            topbar.removeClass('top-bar-top');
 		}
+
 	});
-	window.scrolltopost0 = function(){
-    	$('html,body').animate({
-    		scrollTop: $('#post0').offset().top
-    	}, 500);
-    		
-    }
 
-	window.scrolltopost1 = function(){
-	//console.log('an');
-		$('html,body').animate({
-       		scrollTop: $('#post1').offset().top-53
-   		}, 500);
-    }
+    $('.button.menu').click(function(){
+        if (topbar.height() == 53) {
+            topbar.animate({
+                height: topbar[0].scrollHeight+'px'
+            }, 300);
+        } else {
+            topbar.animate({
+                height: "53px"
+            }, 300);
+        }
+    })
 
-    window.scrolltopost2 = function(){
-    	$('html,body').animate({
-    		scrollTop: $('#post2').offset().top-53
-    	}, 500);
-    		
-    }
-
-    window.scrolltopost3 = function(){
-    	$('html,body').animate({
-    		scrollTop: $('#post3').offset().top-53
-    	}, 500);
-    		
-    }
-
-    window.scrolltopost4 = function(){
-    	$('html,body').animate({
-    		scrollTop: $('#post4').offset().top-53
-    	}, 500);
-    		
+    window.scrolltopost = function(data){
+        $('html,body').animate({
+            scrollTop: $('[data-slide='+data+']').offset().top-53
+        }, 500);
     }
 
 });
